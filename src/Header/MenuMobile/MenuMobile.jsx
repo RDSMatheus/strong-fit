f4f7a4db91e8f4799482e04460d05e5f7d866902
RDSMatheus <matheus_rds1998@hotmail.com>
2023-11-05 10:29:39 -0300

    menu hamburguer adicionado, alguns problemas ainda

--- src/Header/MenuMobile/MenuMobile.jsx
+++ src/Header/MenuMobile/MenuMobile.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import styles from './MenuMobile.module.scss';
+import MenuHamburguer from '../../assets/menu-hamburguer.svg';
+import CloseMenu from '../../assets/fechar.svg';
 
 const MenuMobile = () => {
   const [hamburguer, setHamburguer] = React.useState(false);
   return (
-    <div>
-      <button onClick={() => setHamburguer(!hamburguer)}>abrir</button>
+    <>
+      <button
+        className={styles.openMenuMobile}
+        onClick={() => setHamburguer(!hamburguer)}
+      >
+        <img src={MenuHamburguer} alt="Icone do menu hamburguer" />
+      </button>
       {hamburguer && (
-        <nav className={styles.menuMobile}>
-          <ul>
-            <li>Nossas Instalações</li>
-            <li>Nossas Instalações</li>
-            <li>Nossas Instalações</li>
-            <li>Nossas Instalações</li>
-          </ul>
-        </nav>
+        <div className={styles.menuMobileContainer}>
+          <nav className={styles.menuMobile}>
+            <ul>
+              <li>Nossas Instalações</li>
+              <li>Nossas Instalações</li>
+              <li>Nossas Instalações</li>
+              <li>Nossas Instalações</li>
+            </ul>
+            <button
+              className={styles.closeMenuMobile}
+              onClick={() => setHamburguer(!hamburguer)}
+            >
+              <img src={CloseMenu} alt="Icone fechar menu hamburguer" />
+            </button>
+          </nav>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
