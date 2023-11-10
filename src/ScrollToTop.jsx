9d9f4acc2240c48e84ee783093df55b8aecc14de
RDSMatheus <matheus_rds1998@hotmail.com>
2023-11-10 11:35:25 -0300

    bug fix safari iphone

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
   const location = useLocation();
 
   React.useEffect(() => {
-    window.scrollTo(100, 0);
+    window.scrollTo(0, 0);
   }, [location]);
 
   return null;
